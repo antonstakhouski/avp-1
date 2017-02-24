81f8f02d79020b7ed2ae783695efe6961653de65
Anton Stakhouski <antonstakhouski@gmail.com>
2017-02-24 23:53:16 +0300

    advanced matrix multiplication added

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CC = gcc
-CFLAGS = -c -std=c11 -save-temps -fverbose-asm -Wall -masm=intel -O2
+CFLAGS = -c -save-temps -O2 -fverbose-asm -Wall -masm=intel
 OUTPUT_NAME = lab
-BUILD = $(CC) -lrt main.o -o $(OUTPUT_NAME)
+BUILD = $(CC) main.o -o $(OUTPUT_NAME)
 
 all: no-vect
 no-vect: main.o-no-vect
--- main.c
+++ main.c
@@ -1,84 +1,133 @@
+#define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
 
-#define n 4
-#define pi 3.14
+#define N 1024
+#define PI 3.14
 
 struct timespec diff(struct timespec start, struct timespec end);
 
 float** matrix_init() {
-	float** matrix;
-	matrix = (float**)malloc(sizeof(float*)*n);
-	for (int i = 0; i < n; i++) {
-		matrix[i] = (float*)malloc(sizeof(float)*n);
-		for (int j = 0; j < n; j++) {
-				matrix[i][j] = (float)(rand() % 100) / pi;
-			}
-		}
-	return matrix;
+    float** mat;
+    struct timespec tmp;
+    mat = (float**)malloc(sizeof(float*) * N);
+    for (int i = 0; i < N; i++) {
+        mat[i] = (float*)malloc(sizeof(float) * N);
+        for (int j = 0; j < N; j++) {
+            clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &tmp);
+            srand(tmp.tv_nsec);
+            mat[i][j] = (float)(rand() % 100) / PI;
+        }
+    }
+    return mat;
 }
 
-float** matrix_init_zero() {
-	float** matrix;
-	matrix = (float**)malloc(sizeof(float*)*n);
-	for (int i = 0; i < n; i++) {
-		matrix[i] = (float*)calloc(n, sizeof(float));
-	}
-	return matrix;
+void matrix_zero(float** mat) {
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            mat[i][j] = 0;
+        }
+    }
 }
+
+
 
 struct timespec diff(struct timespec start, struct timespec end) {
-	struct timespec temp;
-	if ((end.tv_nsec - start.tv_nsec)<0) {
-		temp.tv_sec = end.tv_sec - start.tv_sec - 1;
-		temp.tv_nsec = 1000000000 + end.tv_nsec - start.tv_nsec;
-	}
-	else {
-		temp.tv_sec = end.tv_sec - start.tv_sec;
-		temp.tv_nsec = end.tv_nsec - start.tv_nsec;
-	}
-	return temp;
+    struct timespec temp;
+    if ((end.tv_nsec - start.tv_nsec)<0) {
+        temp.tv_sec = end.tv_sec - start.tv_sec - 1;
+        temp.tv_nsec = 1000000000 + end.tv_nsec - start.tv_nsec;
+    }
+    else {
+        temp.tv_sec = end.tv_sec - start.tv_sec;
+        temp.tv_nsec = end.tv_nsec - start.tv_nsec;
+    }
+    return temp;
 }
 
-float** matrix_mul(float** a, float** b) {
-	float** c;
-	c = matrix_init_zero();
+void matrix_naive(float** c, float** a, float** b)
+{
+    matrix_zero(c);
     struct timespec time1, time2, res;
-	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
-	
-	for (int i = 0; i < n; i++)
-		for (int j = 0; j < n; j++) 
-			for (int r = 0; r < n; r++)
-				c[i][j] += a[i][r] * b[r][j];
-    
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+
+    for (int i = 0; i < N; i++)
+        for (int j = 0; j < N; j++) 
+            for (int r = 0; r < N; r++)
+                c[i][j] += a[i][r] * b[r][j];
+
     clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
-	res = diff(time1,time2);
-	printf("%lld.", (long long)res.tv_sec);
-	printf("%ld\n", res.tv_nsec);
-	return c;
+    res = diff(time1,time2);
+    dprintf(2, "naive: %lld.", (long long)res.tv_sec);
+    dprintf(2, "%ld\n", res.tv_nsec);
+}  
+
+void dgemm(float** c, float** a, float** b)
+{
+    matrix_zero(c);
+    struct timespec time1, time2, res;
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time1);
+
+    int ax = 0;
+    int ay = 0;
+    for(int cy = 0; cy < N; cy++ )
+    {
+        for(int by = 0; by < N; by++)
+        { 
+            for(int bx = 0, cx = 0; bx < N && cx < N; bx++, cx++)
+            { 
+                c[cy][cx] += a[ay][ax] * b[by][bx];
+
+            }
+
+            if(ax + 1 < N)
+            {
+                ax++;
+            }
+            else
+            {
+                ax = 0;
+                ay++;
+            }
+        }
+    }
+
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &time2);
+    res = diff(time1,time2);
+    dprintf(2, "advanced: %lld.", (long long)res.tv_sec);
+    dprintf(2, "%ld\n", res.tv_nsec);
 }
 
 void print_mat(float** mat) {
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < n; j++) {
-			printf("%f ", mat[i][j]);
-		}
-		printf("\n");
-	}
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < N; j++) {
+            printf("%f ", mat[i][j]);
+        }
+        printf("\n");
+    }
 }
 
 int main() {
-	float** matrix_a;
-	float** matrix_b;
-	float** matrix_c;
+    float** matrix_a;
+    float** matrix_b;
+    float** matrix_c;
 
-	matrix_a = matrix_init();
-	matrix_b = matrix_init();
-	matrix_c = matrix_init();
+    matrix_a = matrix_init();
+    matrix_b = matrix_init();
+    matrix_c = matrix_init();
+    /** printf("%f", matrix_b[0][0]); */
 
-	matrix_c = matrix_mul(matrix_a, matrix_b);
-	print_mat(matrix_c);
+    /** matrix_a = matrix_init(); */
+    /** matrix_b = matrix_init(); */
+    /** matrix_c = matrix_init(); */
 
-	return 0;
+    /** matrix_naive(matrix_c, matrix_a, matrix_b); */
+    /** print_mat(matrix_c); */
+
+    dgemm(matrix_c, matrix_a, matrix_b);
+    /** print_mat(matrix_c);  */
+
+    return 0;
 }
